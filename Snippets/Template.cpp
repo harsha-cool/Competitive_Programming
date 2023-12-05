df0da7fd218517d083dede364df01e2e4aec1011
sriharshamoparthy <105986642+harsha-cool@users.noreply.github.com>
2023-12-05 18:35:40 +0530

    Update Template.cpp

--- Snippets/Template.cpp
+++ Snippets/Template.cpp
@@ -83,188 +83,190 @@ template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 // also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
 constexpr int pct(int x) { return __builtin_popcount(x); }  // # of bits set
 constexpr int bits(int x) {  // assert(x >= 0); // make C++11 compatible until
-	                         // USACO updates ...
-	return x == 0 ? 0 : 31 - __builtin_clz(x);
+    // USACO updates ...
+    return x == 0 ? 0 : 31 - __builtin_clz(x);
 }  // floor(log2(x))
 constexpr int p2(int x) { return 1 << x; }
 constexpr int msk2(int x) { return p2(x) - 1; }
 
 ll cdiv(ll a, ll b) {
-	return a / b + ((a ^ b) > 0 && a % b);
+    return a / b + ((a ^ b) > 0 && a % b);
 }  // divide a by b rounded up
 ll fdiv(ll a, ll b) {
-	return a / b - ((a ^ b) < 0 && a % b);
+    return a / b - ((a ^ b) < 0 && a % b);
 }  // divide a by b rounded down
 
 tcT > bool ckmin(T &a, const T &b) {
-	return b < a ? a = b, 1 : 0;
+    return b < a ? a = b, 1 : 0;
 }  // set a = min(a,b)
 tcT > bool ckmax(T &a, const T &b) {
-	return a < b ? a = b, 1 : 0;
+    return a < b ? a = b, 1 : 0;
 }  // set a = max(a,b)
 
 tcTU > T fstTrue(T lo, T hi, U f) {
-	++hi;
-	assert(lo <= hi);  // assuming f is increasing
-	while (lo < hi) {  // find first index such that f is true
-		T mid = lo + (hi - lo) / 2;
-		f(mid) ? hi = mid : lo = mid + 1;
-	}
-	return lo;
+    ++hi;
+    assert(lo <= hi);  // assuming f is increasing
+    while (lo < hi) {  // find first index such that f is true
+        T mid = lo + (hi - lo) / 2;
+        f(mid) ? hi = mid : lo = mid + 1;
+    }
+    return lo;
 }
 tcTU > T lstTrue(T lo, T hi, U f) {
-	--lo;
-	assert(lo <= hi);  // assuming f is decreasing
-	while (lo < hi) {  // find first index such that f is true
-		T mid = lo + (hi - lo + 1) / 2;
-		f(mid) ? lo = mid : hi = mid - 1;
-	}
-	return lo;
+    --lo;
+    assert(lo <= hi);  // assuming f is decreasing
+    while (lo < hi) {  // find first index such that f is true
+        T mid = lo + (hi - lo + 1) / 2;
+        f(mid) ? lo = mid : hi = mid - 1;
+    }
+    return lo;
 }
 tcT > void remDup(vector<T> &v) {  // sort and remove duplicates
-	sort(all(v));
-	v.erase(unique(all(v)), end(v));
+    sort(all(v));
+    v.erase(unique(all(v)), end(v));
 }
 tcTU > void safeErase(T &t, const U &u) {
-	auto it = t.find(u);
-	assert(it != end(t));
-	t.erase(it);
+    auto it = t.find(u);
+    assert(it != end(t));
+    t.erase(it);
 }
 
 inline namespace IO {
-#define SFINAE(x, ...)                                                         
-	template <class, class = void> struct x : std::false_type {};              
+#define SFINAE(x, ...)                                                         \
+	template <class, class = void> struct x : std::false_type {};              \
 	template <class T> struct x<T, std::void_t<__VA_ARGS__>> : std::true_type {}
 
-SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>()));
-SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
-SFINAE(IsTuple, typename std::tuple_size<T>::type);
-SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
+    SFINAE(DefaultI, decltype(std::cin >> std::declval<T &>()));
+    SFINAE(DefaultO, decltype(std::cout << std::declval<T &>()));
+    SFINAE(IsTuple, typename std::tuple_size<T>::type);
+    SFINAE(Iterable, decltype(std::begin(std::declval<T>())));
 
-template <auto &is> struct Reader {
-	template <class T> void Impl(T &t) {
-		if constexpr (DefaultI<T>::value) is >> t;
-		else if constexpr (Iterable<T>::value) {
-			for (auto &x : t) Impl(x);
-		} else if constexpr (IsTuple<T>::value) {
-			std::apply([this](auto &...args) { (Impl(args), ...); }, t);
-		} else static_assert(IsTuple<T>::value, "No matching type for read");
-	}
-	template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
-};
+    template <auto &is> struct Reader {
+        template <class T> void Impl(T &t) {
+            if constexpr (DefaultI<T>::value) is >> t;
+            else if constexpr (Iterable<T>::value) {
+                for (auto &x : t) Impl(x);
+            } else if constexpr (IsTuple<T>::value) {
+                std::apply([this](auto &...args) { (Impl(args), ...); }, t);
+            } else static_assert(IsTuple<T>::value, "No matching type for read");
+        }
+        template <class... Ts> void read(Ts &...ts) { ((Impl(ts)), ...); }
+    };
 
-template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
-#define def(t, args...)                                                        
-	t args;                                                                    
+    template <class... Ts> void re(Ts &...ts) { Reader<cin>{}.read(ts...); }
+#define def(t, args...)                                                        \
+	t args;                                                                    \
 	re(args);
 
-template <auto &os, bool debug, bool print_nd> struct Writer {
-	string comma() const { return debug ? "," : ""; }
-	template <class T> constexpr char Space(const T &) const {
-		return print_nd && (Iterable<T>::value or IsTuple<T>::value) ? '\n'
-		                                                             : ' ';
-	}
-	template <class T> void Impl(T const &t) const {
-		if constexpr (DefaultO<T>::value) os << t;
-		else if constexpr (Iterable<T>::value) {
-			if (debug) os << '{';
-			int i = 0;
-			for (auto &&x : t)
-				((i++) ? (os << comma() << Space(x), Impl(x)) : Impl(x));
-			if (debug) os << '}';
-		} else if constexpr (IsTuple<T>::value) {
-			if (debug) os << '(';
-			std::apply(
-			    [this](auto const &...args) {
-				    int i = 0;
-				    (((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)),
-				     ...);
-			    },
-			    t);
-			if (debug) os << ')';
-		} else static_assert(IsTuple<T>::value, "No matching type for print");
-	}
-	template <class T> void ImplWrapper(T const &t) const {
-		if (debug) os << "\033[0;31m";
-		Impl(t);
-		if (debug) os << "\033[0m";
-	}
-	template <class... Ts> void print(Ts const &...ts) const {
-		((Impl(ts)), ...);
-	}
-	template <class F, class... Ts>
-	void print_with_sep(const std::string &sep, F const &f,
-	                    Ts const &...ts) const {
-		ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
-	}
-	void print_with_sep(const std::string &) const { os << '\n'; }
-};
+    template <auto &os, bool debug, bool print_nd> struct Writer {
+        string comma() const { return debug ? "," : ""; }
+        template <class T> constexpr char Space(const T &) const {
+            return print_nd && (Iterable<T>::value or IsTuple<T>::value) ? '\n'
+                                                                         : ' ';
+        }
+        template <class T> void Impl(T const &t) const {
+            if constexpr (DefaultO<T>::value) os << t;
+            else if constexpr (Iterable<T>::value) {
+                if (debug) os << '{';
+                int i = 0;
+                for (auto &&x : t)
+                    ((i++) ? (os << comma() << Space(x), Impl(x)) : Impl(x));
+                if (debug) os << '}';
+            } else if constexpr (IsTuple<T>::value) {
+                if (debug) os << '(';
+                std::apply(
+                        [this](auto const &...args) {
+                            int i = 0;
+                            (((i++) ? (os << comma() << " ", Impl(args)) : Impl(args)),
+                                    ...);
+                        },
+                        t);
+                if (debug) os << ')';
+            } else static_assert(IsTuple<T>::value, "No matching type for print");
+        }
+        template <class T> void ImplWrapper(T const &t) const {
+            if (debug) os << "\033[0;31m";
+            Impl(t);
+            if (debug) os << "\033[0m";
+        }
+        template <class... Ts> void print(Ts const &...ts) const {
+            ((Impl(ts)), ...);
+        }
+        template <class F, class... Ts>
+        void print_with_sep(const std::string &sep, F const &f,
+                            Ts const &...ts) const {
+            ImplWrapper(f), ((os << sep, ImplWrapper(ts)), ...), os << '\n';
+        }
+        void print_with_sep(const std::string &) const { os << '\n'; }
+    };
 
-template <class... Ts> void pr(Ts const &...ts) {
-	Writer<cout, false, true>{}.print(ts...);
-}
-template <class... Ts> void ps(Ts const &...ts) {
-	Writer<cout, false, true>{}.print_with_sep(" ", ts...);
-}
+    template <class... Ts> void pr(Ts const &...ts) {
+        Writer<cout, false, true>{}.print(ts...);
+    }
+    template <class... Ts> void ps(Ts const &...ts) {
+        Writer<cout, false, true>{}.print_with_sep(" ", ts...);
+    }
 }  // namespace IO
 
 inline namespace Debug {
-template <typename... Args> void err(Args... args) {
-	Writer<cerr, true, false>{}.print_with_sep(" | ", args...);
-}
-template <typename... Args> void errn(Args... args) {
-	Writer<cerr, true, true>{}.print_with_sep(" | ", args...);
-}
+    template <typename... Args> void err(Args... args) {
+        Writer<cerr, true, false>{}.print_with_sep(" | ", args...);
+    }
+    template <typename... Args> void errn(Args... args) {
+        Writer<cerr, true, true>{}.print_with_sep(" | ", args...);
+    }
 
-void err_prefix(str func, int line, string args) {
-	cerr << "\033[0;31m\u001b[1mDEBUG\033[0m"
-	     << " | "
-	     << "\u001b[34m" << func << "\033[0m"
-	     << ":"
-	     << "\u001b[34m" << line << "\033[0m"
-	     << " - "
-	     << "[" << args << "] = ";
-}
+    void err_prefix(str func, int line, string args) {
+        cerr << "\033[0;31m\u001b[1mDEBUG\033[0m"
+             << " | "
+             << "\u001b[34m" << func << "\033[0m"
+             << ":"
+             << "\u001b[34m" << line << "\033[0m"
+             << " - "
+             << "[" << args << "] = ";
+    }
 
 #ifdef LOCAL
-#define dbg(args...) err_prefix(__FUNCTION__, __LINE__, #args), err(args)
+    #define dbg(args...) err_prefix(__FUNCTION__, __LINE__, #args), err(args)
 #define dbgn(args...) err_prefix(__FUNCTION__, __LINE__, #args), errn(args)
 #else
 #define dbg(...)
 #define dbgn(args...)
 #endif
 
-const auto beg_time = std::chrono::high_resolution_clock::now();
+    const auto beg_time = std::chrono::high_resolution_clock::now();
 // https://stackoverflow.com/questions/47980498/accurate-c-c-clock-on-a-multi-core-processor-with-auto-overclock?noredirect=1&lq=1
-double time_elapsed() {
-	return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
-	                                beg_time)
-	    .count();
-}
+    double time_elapsed() {
+        return chrono::duration<double>(std::chrono::high_resolution_clock::now() -
+                                        beg_time)
+                .count();
+    }
 }  // namespace Debug
 
 inline namespace FileIO {
-void setIn(str s) { freopen(s.c_str(), "r", stdin); }
-void setOut(str s) { freopen(s.c_str(), "w", stdout); }
-void setIO(str s = "") {
-	cin.tie(0)->sync_with_stdio(0);  // unsync C / C++ I/O streams
-	cout << fixed << setprecision(12);
-	// cin.exceptions(cin.failbit);
-	// throws exception when do smth illegal
-	// ex. try to read letter into int
-	if (sz(s)) setIn(s + ".in"), setOut(s + ".out");  // for old USACO
-}
+    void setIn(str s) { freopen(s.c_str(), "r", stdin); }
+    void setOut(str s) { freopen(s.c_str(), "w", stdout); }
+    void setIO(str s = "") {
+        cin.tie(0)->sync_with_stdio(0);  // unsync C / C++ I/O streams
+        cout << fixed << setprecision(12);
+        // cin.exceptions(cin.failbit);
+        // throws exception when do smth illegal
+        // ex. try to read letter into int
+        if (sz(s)) setIn(s + ".in"), setOut(s + ".out");  // for old USACO
+    }
 }  // namespace FileIO
 
 // make sure to intialize ALL GLOBAL VARS between tcs!
+
 void solve(int tc) {
+
 }
 
 int main() {
-	setIO();
-	int TC;
-	re(TC);
-	FOR(i, 1, TC + 1) solve(i);
+    setIO();
+    int TC;
+    re(TC);
+    FOR(i, 1, TC + 1) solve(i);
 }
 
 /* stuff you should look for
