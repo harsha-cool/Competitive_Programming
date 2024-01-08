4ff3abc8f7fe75e780ff7b32d6845aeb5150e6fc
sriharshamoparthy <105986642+harsha-cool@users.noreply.github.com>
2024-01-08 18:58:14 +0530

    Update Heap.cpp

--- Snippets/Heap.cpp
+++ Snippets/Heap.cpp
@@ -3,9 +3,9 @@ void max_heapify(vi &A, int i,int N)
      int left = 2*i+1;
      int right = 2*i+2;
      int largest = 0;
-     if(left<=N && A[left]>A[i]) largest = left;
+     if(left<N && A[left]>A[i]) largest = left;
      else largest = i;
-     if(right<=N && A[right]>A[largest]) largest = right;
+     if(right<N && A[right]>A[largest]) largest = right;
      if(largest!=i)
      {
         swap(A[largest],A[i]);
