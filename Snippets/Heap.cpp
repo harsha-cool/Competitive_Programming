744139a4cb29f86d08be4d018d5c446cbd0aad84
sriharshamoparthy <105986642+harsha-cool@users.noreply.github.com>
2024-01-08 18:49:37 +0530

    Create Heap.cpp

---
+++ Snippets/Heap.cpp
@@ -0,0 +1,19 @@
+void max_heapify(vi &A, int i,int N)
+{
+     int left = 2*i+1;
+     int right = 2*i+2;
+     int largest = 0;
+     if(left<=N && A[left]>A[i]) largest = left;
+     else largest = i;
+     if(right<=N && A[right]>A[largest]) largest = right;
+     if(largest!=i)
+     {
+        swap(A[largest],A[i]);
+        max_heapify(A,largest,N);
+     }
+}
+
+void build_max_heap(vi &A,int N)
+{
+    for(int i=N/2;i>=1;i--) max_heapify(A,i-1,N);
+}
